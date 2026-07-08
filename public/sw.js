40c2b7a00ab1e1a1e876f4497cd959b76d5bf908
MasaArip <ariefsenpai21@gmail.com>
2026-07-08 17:40:09 +0700

    PERBAIKAN QR

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'nyebrang-v2';
-const ASSETS = ['/', '/style.css', '/app.js', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const CACHE = 'nyebrang-v3';
+const ASSETS = ['/', '/style.css', '/app.js', '/qrcode.min.js', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
